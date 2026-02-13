8251b58c6605a409e8e6cbbaee2014266af7578d
Tadeas Maca <maca.tadeas@gmail.com>
2026-02-13 21:49:39 +0100

    bug fixes, room img

--- backend/src/Controller/EnterGameController.php
+++ backend/src/Controller/EnterGameController.php
@@ -4,11 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Session;
 use App\Service\MultiplayerService;
+use App\Service\AssetService;
 
 class EnterGameController extends AbstractPacketController
 {
     public function __construct(
-        private MultiplayerService $multiplayer_service
+        private MultiplayerService $multiplayer_service,
+        private AssetService $asset_service
     ) {}
 
     public function supports(string $type): bool
@@ -29,10 +31,15 @@ class EnterGameController extends AbstractPacketController
         }
 
         $this->multiplayer_service->join_room($session);
-            
+
+        $player = $session->data->player;
         $session->send([
-            'type' => 'server_enter_game',
-            'state' =>'success',
-        ]);
+                'type' => 'server_place',
+                'place' => [
+                    'img' => $player->getRoomImg() ?? $this->asset_service->getRoomDefault(),
+                    'id' => $player->getPlayerId(),
+                    'is_floor' => false
+                ]
+            ]);
     }
 }
--- backend/src/Service/MultiplayerService.php
+++ backend/src/Service/MultiplayerService.php
@@ -5,13 +5,15 @@ namespace App\Service;
 use App\Entity\Player;
 use App\Entity\Session;
 
+use App\Service\AssetService;
+
 class MultiplayerService
 {
     private int $room_count = 5;
     private array $game = [];
 
     public function __construct(
-       
+        private AssetService $asset_service
     ) {}
 
     public function join_room(Session $session)
@@ -28,18 +30,21 @@ class MultiplayerService
             $this->game[$room_id] = [];
         }
 
-        $session->on_disconnect = function() use ($player) {
-            unset($this->game[$player->getPlayerId()]);
+        $session->on_disconnect = function() use ($session, $room_id) {
+            $key = array_search($session, $this->game[$room_id], true);
+            if ($key !== false) {
+                unset($this->game[$room_id][$key]);
+            }
         };
 
-        array_push($this->game[$room_id], $player);
-
-        foreach($this->game as $s) {
+        foreach($this->game[$room_id] as $s) {
             $s->send([
                 'type' => 'server_player_join',
                 'player_id' => $s->data->player->getPlayerId(),
-                'img' => $s->data->player->getImg() ?? 'TODO: add url here'
+                'img' => $s->data->player->getImg() ?? $this->asset_service->getPlayerDefault()
             ]);
         }
+        
+        array_push($this->game[$room_id], $session);
     }
 }
